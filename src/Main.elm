5b8ccf61600031f92402b104a1439be3c03206c8
edamame <edamame15.r@gmail.com>
2022-05-12 21:19:21 +0900

    DayTimeを使うよう修正

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,10 @@
 module Main exposing (..)
 
 import Browser
+import DayTime exposing (DayTime)
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Task exposing (perform)
-import Time exposing (Posix)
 
 
 
@@ -12,8 +12,7 @@ import Time exposing (Posix)
 
 
 type alias Model =
-    { log : List Posix
-    , zone : Maybe Time.Zone
+    { log : List DayTime
     }
 
 
@@ -23,8 +22,11 @@ type alias Model =
 
 type Msg
     = AddLogReq
-    | AddLog Posix
-    | ReceiveZone Time.Zone
+    | AddLog DayTime
+
+
+
+-- INIT
 
 
 init : a -> ( Model, Cmd Msg )
@@ -32,23 +34,23 @@ init _ =
     let
         model =
             { log = []
-            , zone = Nothing
             }
     in
-    ( model, perform ReceiveZone Time.here )
+    ( model, Cmd.none )
+
+
+
+-- UPDATE
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AddLogReq ->
-            ( model, perform AddLog Time.now )
+            ( model, perform AddLog DayTime.now )
 
         AddLog time ->
             ( { model | log = time :: model.log }, Cmd.none )
-
-        ReceiveZone zone ->
-            ( { model | zone = Just zone }, Cmd.none )
 
 
 
@@ -57,43 +59,15 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    let
-        logView =
-            case model.zone of
-                Nothing ->
-                    []
-
-                Just zone ->
-                    List.map (viewTimeLog zone) model.log
-    in
     div []
         [ button [ onClick AddLogReq ] [ text "save" ]
-        , div [] logView
+        , div [] <| List.map viewTimeLog model.log
         ]
 
 
-viewTimeLog : Time.Zone -> Posix -> Html Msg
-viewTimeLog zone log =
-    div [] [ text (formatPosix zone log) ]
-
-
-formatPosix : Time.Zone -> Posix -> String
-formatPosix zone posix =
-    let
-        toStringWithPad : Int -> String
-        toStringWithPad =
-            String.fromInt >> String.padLeft 2 '0'
-
-        hour =
-            toStringWithPad <| Time.toHour zone posix
-
-        minute =
-            toStringWithPad <| Time.toMinute zone posix
-
-        second =
-            toStringWithPad <| Time.toSecond zone posix
-    in
-    String.join ":" [ hour, minute, second ]
+viewTimeLog : DayTime -> Html Msg
+viewTimeLog time =
+    div [] [ text (DayTime.show time) ]
 
 
 
